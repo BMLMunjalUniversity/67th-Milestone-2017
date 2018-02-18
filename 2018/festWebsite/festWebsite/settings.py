b04d75b3b87f5a4142e27c3368e4e16ed2c21832
Prashantpandey2398 <pprashant2398@gmail.com>
2018-02-18 23:09:21 +0530

    Campuss Ambassdor form page added

--- 2018/festWebsite/festWebsite/settings.py
+++ 2018/festWebsite/festWebsite/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(__file__)
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,12 +55,13 @@ ROOT_URLCONF = 'festWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR,]
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- 2018/festWebsite/website/views.py
+++ 2018/festWebsite/website/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from website.models import Campus_Ambassdor
+from website.forms import Campus_Ambassdor_Form
 # Create your views here.
+def index(request):
+    context_dict = {}
+    return render(request,'website/index.html',context_dict)
+
+
+def campusambassdor(request):
+    form = Campus_Ambassdor_Form()
+    if request.method == 'POST':
+        form = Campus_Ambassdor_Form(request.POST)
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.save()
+        else:
+            print(form.errors)
+    context_dict = {'form': form}
+    return render(request, 'website/Campus_Ambassdor_htmlform.html', context_dict)
+
+
+def success(request):
+    context_dict={}
+    return render(request, 'website/Success.html', context_dict)
